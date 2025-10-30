adc2eeb64766de220644f8590459de08b319ff53
tuleshwar <tuleshwarsahu94079@gmail.com>
2025-10-30 20:00:45 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="light">
       <body className={`${geistSans.className} antialiased`}>{children}</body>
     </html>
   )
